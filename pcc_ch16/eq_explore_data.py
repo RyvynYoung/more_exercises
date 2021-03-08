3a9f0f3ccac67f4bf0575dcf2cb13bc9d8bd154f
RyvynYoung <ryvyn.young@gmail.com>
2021-03-08 17:46:34 -0600

    extract needed data

--- pcc_ch16/eq_explore_data.py
+++ pcc_ch16/eq_explore_data.py
@@ -5,6 +5,26 @@ filename =  'codeup-data-science/more_exercises/pcc_ch16/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
-readable_file = 'codeup-data-science/more_exercises/pcc_ch16/readable_eq_data.json'
-with open(readable_file, 'w') as f:
-    json.dump(all_eq_data, f, indent=4)
+# readable_file = 'codeup-data-science/more_exercises/pcc_ch16/readable_eq_data.json'
+# with open(readable_file, 'w') as f:
+#     json.dump(all_eq_data, f, indent=4)
+
+# get a list that contains all of the information about every earthquake that occurred
+all_eq_dicts = all_eq_data['features']
+# print(len(all_eq_dicts))
+
+# extract magnitudes and location for each earthquake
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+# print(mags[:10])
+print(lons[:5])
+print(lats[:5])
+
+
+
