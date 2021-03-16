aa9279838119eb738672057305d1da4961c190c7
RyvynYoung <ryvyn.young@gmail.com>
2021-03-16 14:19:48 -0500

    refactored code per 16-6 exercise

--- pcc_ch16/eq_world_map.py
+++ pcc_ch16/eq_world_map.py
@@ -16,16 +16,23 @@ all_eq_dicts = all_eq_data['features']
 # print(len(all_eq_dicts))
 
 # extract magnitudes and location for each earthquake
+# refactored code
 mags, lons, lats, hover_texts = [], [], [], []
-for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    title = eq_dict['properties']['title']
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
-    hover_texts.append(title)
+mags = [eq_dict['properties']['mag'] for eq_dict in all_eq_dicts]
+lons = [eq_dict['geometry']['coordinates'][0] for eq_dict in all_eq_dicts]
+lats = [eq_dict['geometry']['coordinates'][1] for eq_dict in all_eq_dicts]
+hover_texts = [eq_dict['properties']['title'] for eq_dict in all_eq_dicts]
+
+# original code
+# for eq_dict in all_eq_dicts:
+#     mag = eq_dict['properties']['mag']
+#     lon = eq_dict['geometry']['coordinates'][0]
+#     lat = eq_dict['geometry']['coordinates'][1]
+#     title = eq_dict['properties']['title']
+#     mags.append(mag)
+#     lons.append(lon)
+#     lats.append(lat)
+#     hover_texts.append(title)
 # print(mags[:10])
 # print(lons[:5])
 # print(lats[:5])
